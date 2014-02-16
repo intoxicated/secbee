39c5e4ee7227bc227699817f20dcbd9a594d539a
Wooyoung Chung <yakle1218@gmail.com>
2014-02-15 22:59:52 -0500

    Response class documentation finish

--- c1222.cpp
+++ c1222.cpp
@@ -77,8 +77,6 @@ c1222_packet::build()
 
 }
 
-
-
 /* ========================================================================= *
  *                                                                           *
  *                                 ACSE class                                *  
@@ -120,9 +118,9 @@ c1222_request::clear()
 }
 
 /**
- *  Identity request 
- *  Detail : Obtain device functionality 
- *  Header : 0x20
+ * Identity request 
+ * Detail : Obtain device functionality 
+ * Header : 0x20
  */
 void
 c1222_request::identify()
@@ -358,6 +356,185 @@ c1222_request::resolve(const byte * ap_title)
  */
 void
 c1222_request::trace(const byte * ap_title)
+{
+
+}
+
+
+/* ======================================================================== *
+ *                                                                          *
+ *                            C1222 response clas                           *  
+ *                                                                          *
+ * ======================================================================== */
+
+void *
+c1222_response::build(void)
+{
+    return raw;
+}
+
+void 
+c1222_response::clear(void)
+{
+    delete raw;
+}
+
+/**
+ * Identify response
+ * 
+ * @param res response isss or bsy or err or ok with parameters
+ * @param std represent standard type of device (byte)
+ *              0x00 : ANSI C12.18
+ *              0x01 : Reserve
+ *              0x02 : ANSI C12.21
+ *              0x03 : ANSI C12.22
+ *              0x04 - 0xFF : Reserve
+ * @param ver binary representation of standard version left of decimal pts 
+ * @param rev binary representation of standard version right of decimal pts
+ */
+void
+c1222_response::identify(const byte res, const byte std, const byte ver,
+        const byte rev)
+{
+
+}
+
+/**
+ * TBD
+ *
+ */
+void 
+c1222_response::read(const byte res, const byte * count, const byte * data,
+        const byte chksum)
+{
+
+}
+
+/**
+ * TBD
+ *
+ */
+void
+c1222_response::write(const byte res)
+{
+
+}
+
+/**
+ * Logon response
+ *
+ * @param res       response state 
+ * @param timeout   seconds to be idle before termination (word16)
+ */
+void
+c1222_response::logon(const byte res, const byte * timeout)
+{
+
+}
+
+/**
+ * Security response
+ *
+ * @param res       response state
+ */
+void
+c1222_response::security(const byte res)
+{
+
+}
+
+/**
+ * Logoff response
+ *
+ * @param res       response state
+ */
+void
+c1222_response::logoff(const byte res)
+{
+
+}
+
+/**
+ * Terminate response
+ *
+ * @param res       response state
+ */
+void
+c1222_response::terminate(const byte res)
+{
+
+}
+
+/**
+ * Disconnect response
+ *
+ * @param res       response state
+ */
+void
+c1222_response::disconnect(const byte res)
+{
+
+}
+
+/**
+ * Wait response
+ *
+ * @param res       response state
+ */
+void
+c1222_response::wait(const byte res)
+{
+
+}
+
+/**
+ * Registration response
+ *
+ * @param res       response state
+ * @param ap_title  registred ap title
+ * @param delay     maximum delay in seconds that device should waits
+ * @param period    maximum period in seconds allowed to elapse re-reg
+ * @param info      Node info about connection type (see Reg. request)
+ */
+void
+c1222_response::registration(const byte res, const byte * ap_title,
+        const byte * delay, const byte * period, const byte info)
+{
+
+}
+
+/**
+ * Deregistration response
+ *
+ * @param res       response state
+ */
+void
+c1222_response::deregistration(const byte res)
+{
+
+}
+
+/**
+ * Resolve response
+ *
+ * @param res       response state
+ * @param addr_len  address length 
+ * @param addr      local address of requested aptitle
+ */
+void
+c1222_response::resolve(const byte res, const byte addr_len, const byte * addr)
+{
+
+}
+
+/**
+ * Resolve response
+ *
+ * @param res       response state
+ * @param ap_titles aptitle of c12.22 relays used to forward this request
+ */
+void
+trace(const byte res, const byte ** aptitles)
 {
 
 }
--- c1222.h
+++ c1222.h
@@ -25,8 +25,8 @@ class c1222_packet : public c1222 {
     
     private:
         void * data;
-        char * called_ap, * called_ap_invo;
-        char * calling_ap, * calling_ap_invo;
+        char * called_ap, * called_ap_invo; //destination 
+        char * calling_ap, * calling_ap_invo; //source
         int length;
 };
 
@@ -70,7 +70,6 @@ class c1222_request : public c1222 {
         void * build();
         void clear();
 
-        //request methods
         void disconnect(void);
         
         void identify(void);
@@ -108,10 +107,59 @@ class c1222_request : public c1222 {
         byte * raw;
 };
 
+
+/**
+ * response class (purpose to work on prototype) 
+ *
+ */
+
+//minimal response code
+#define RES_OK      '\x00' //ok 
+#define RES_ERR     '\x01' //error 
+#define RES_SNS     '\x02' //service not support
+#define RES_ONP     '\x04' //operation not possible 
+#define RES_BSY     '\x06' //device busy
+#define RES_ISSS    '\x0A' //invalid serviece sequence state
+#define RES_SME     '\x0B' //security mechanism error 
+#define RES_UAT     '\x0C' //unknown application title (aptitle)
+#define RES_SGNP    '\x11' //segment not possible
+
+
 class c1222_response : public c1222 {
     public:
-        void * build();
-        void clear();
+        void * build(void);
+        void clear(void);
     
-        //response methods
+        //feature excluded but necessary to add..
+        void identify(const byte res, const byte std, const byte ver, 
+                const byte rev);  
+
+        void read(const byte res, const byte * count, const byte * data,
+                const byte chksum);
+
+        void write(const byte res);
+
+        void logon(const byte res, const byte * timeout);
+
+        void security(const byte res);
+
+        void logoff(const byte res);
+
+        void terminate(const byte res);
+
+        void disconnect(const byte res);
+
+        void wait(const byte res);
+
+        void registration(const byte res, const byte * ap_title, 
+                const byte * delay, const byte * period, const byte info);
+
+        void deregistration(const byte res);
+
+        void resolve(const byte res, const byte addr_len, const byte * addr);
+
+        void trace(const byte res, const byte ** aptitles);
+
+    private:
+        byte * raw; 
 };
