d7adcde9c441477ae4e22bf122316d79b7c33cdd
Wooyoung Chung <yakle1218@gmail.com>
2014-02-20 21:58:55 -0500

    ACSE header defined, need documentation

--- Protocol/C1222/C1222_ACSE.h
+++ Protocol/C1222/C1222_ACSE.h
@@ -8,17 +8,27 @@
 #ifndef __C1222_ACSE_H__
 #define __C1222_ACSE_H__
 
+#include <stdint.h>
 #include "C1222.h"
 /**
  * ACSE format representation class
  */
-class C1222_acse : public C1222 {
+class C1222_ACSE : public C1222 {
     public:
+        C1222_ACSE(void * usrinfo, uint8_t * calling_title, uint8_t * calling_id,
+                uint8_t * called_title, uint8_t * called_id);
+        ~C1222_ACSE();
+        
+        
         void * build();
-        friend C1222_acse& acse_parse(void * data);
+        friend C1222_acse acse_parse(void * data);
 
     private:
-        char * calling_ap_invo;
+        uint8_t * calling_ap_title, * calling_id;
+        uint8_t * called_ap_title, * called_id;
+        uint8_t * userinfo;
+
+        uint8_t * raw;
 };
 
 #endif
