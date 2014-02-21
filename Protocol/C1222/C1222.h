2233b6d26abe64f17fcfa164cb9698f032dd0e4b
Wooyoung Chung <yakle1218@gmail.com>
2014-02-21 00:28:06 -0500

    introduce container struct data / size

--- Protocol/C1222/C1222.h
+++ Protocol/C1222/C1222.h
@@ -8,8 +8,13 @@
 #ifndef __C1222_BASE_H__
 #define __C1222_BASE_H__
 
-typedef unsigned char uint8_t;
+//typedef unsigned char uint8_t;
+#include <stdint.h>
 
+typedef struct {
+    uint8_t * data;
+    int size;
+} container;
 
 class C1222 {
     virtual void * build() = 0;
