aed8ea8a319070ab370e9cbe491aba210b801ebe
Wooyoung Chung <yakle1218@gmail.com>
2014-03-04 20:00:45 -0500

    Packet (wrapper for ACSE/EPSEM/RES or REP) TBD
    not intend to implement this for now

--- Protocol/C1222/C1222_Packet.cpp
+++ Protocol/C1222/C1222_Packet.cpp
@@ -5,26 +5,12 @@
  * ======================================================================== */
 
 #include "C1222_packet.h"
+
 /**
  * packet methods
  */
-C1222_packet::C1222_packet(char * called_ap, char * called_id,
-        char * calling_ap, char * calling_id, void * data, int len)
+C1222_Packet::C1222_Packet()
 {
-    this->called_ap = called_ap;
-    this->called_ap_invo = called_id;
-    this->calling_ap = calling_ap;
-    this->calling_ap_invo = calling_id;
-
-    this->data = data;
-    this->length = len;
-}
-
-
-C1222_packet& 
-parse(void * data)
-{
-    uint8_t * ptr = (uint8_t *)data;
 
 }
 
--- Protocol/C1222/C1222_Packet.h
+++ Protocol/C1222/C1222_Packet.h
@@ -9,21 +9,46 @@
 #define __C1222-PACKET_H__
 
 #include "C1222.h"
+#include "C1222_ACSE.h"
+#include "C1222_EPSEM.h"
+
+
 /**
  * C1222 packet representation 
  */
-class C1222_packet : public C1222 {
+class C1222_Packet : public C1222 {
     public:
-        C1222_packet(char * called_ap, char * called_id, 
-            char * calling_ap, char * calling_id, void * data, int len);
-        void * build();
-        friend C1222_packet& parse(void * data);
-    
+        C1222_packet();
+
+        uint8_t * build();
+
+        C1222_ACSE * get_acse();
+        C1222_EPSEM * get_epsem();
+        void set_acse(const C1222_ACSE& acseObj);
+        void set_epsem(const C1222_EPSEM& epsemObj);
+
+        //wrapper getters/setters
+        char * get_calling_title();
+        char * get_calling_id();
+        char * get_called_title();
+        char * get_called_id();
+
+        uint8_t * get_epsem_data();
+        void set_data(void * data, long len);
+        long get_length();
+        long get_data_len();
+        uint8_t get_flag(int tag);
+        void set_flags(uint8_t flag);
+
+        static C1222_Packet parse(uint8_t * data);
+
     private:
-        void * data;
-        char * called_ap, * called_ap_invo; //destination 
-        char * calling_ap, * calling_ap_invo; //source
+        void * raw_data;
         int length;
+        uint8_t identifier; //require 
+
+        C1222_ACSE * acse;
+        C1222_EPSEM * epsem;
 };
 
 #endif
