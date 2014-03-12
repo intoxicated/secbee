67236f94101da05219e662a22333323e48d8f6cb
Wooyoung Chung <yakle1218@gmail.com>
2014-03-11 21:24:01 -0400

    Move samples from unittest to sample folder

---
+++ Protocol/C1222/Sample/Request_read.cpp
@@ -0,0 +1,60 @@
+#include "../C1222_ACSE.h"
+#include "../C1222_EPSEM.h"
+#include "../C1222_Request.h"
+
+#include <typeinfo>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int main (int argc, char ** argv)
+{
+
+    uint8_t  data[] = "\x60\x29\xA2\x05\x80\x03\x7B\xC1\x75\
+\xA6\x04\x80\x02\x7B\x04\xA8\x03\x02\x01\x07\
+\xBE\x15\x28\x13\x81\x11\x80\x0F\x50\x00\x02\
+\x55\x53\x45\x52\x20\x4E\x41\x4D\x45\x20\x00\x3C";
+
+    uint8_t data2[] = "\x60\x22\xa2\x04\x80\x02\x7b\x04\
+\xa4\x03\xa6\x05\x80\x03\x7b\xc1\x75\xa8\x03\x02\x01\x07\
+\xbe\x09\x28\x07\x81\x05\x80\x03\x00\x00\x3c";
+
+    C1222_Request_Read read(0x30, 5, NULL, 0,0);
+    uint8_t * d = read.build(); // build data;
+
+    for(int i = 0; i < read.get_build_size(); i++)
+        printf("0x%02x ", d[i]);
+
+    printf("\nrequest data length : %lx\n", read.get_build_size());
+
+    puts("");
+    C1222_EPSEM epsem (d, 0x80, 0, read.get_build_size());
+    d = epsem.build();
+    for(int i = 0; i < epsem.get_length(); ++i)
+        printf("0x%02x ", d[i]);
+
+    printf("\nepsem length : %lx\n", epsem.get_length());
+    puts("");
+
+    C1222_ACSE acse ( d, "123.4", "7", "123.8437", NULL, epsem.get_length());
+    d = acse.build();
+
+    printf("FINAL\n");
+    for(int i = 0; i < acse.get_data_len(); ++i)
+        printf("0x%02x ", d[i]);
+    puts("");
+
+
+    printf("Parse..\n");
+
+    //C1222_Request_Read p = C1222_Request::parse(read.build());
+    void * p = C1222_Request_Parser::parse(read.build());
+    //C1222_Request * p = new C1222_Request_Read();
+    //C1222_Request_Read * r = C1222_Request::parse(read.build());
+    C1222_Request_Read * rp = (C1222_Request_Read *)p;
+    printf("req: 0x%02x tableid: 0x%02x\n", rp->get_request_num(), rp->get_tableid());
+    return 0;
+}
---
+++ Protocol/C1222/Sample/Request_unit.cpp
@@ -0,0 +1,56 @@
+#include "../C1222_ACSE.h"
+#include "../C1222_EPSEM.h"
+#include "../C1222_Request.h"
+
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int main (int argc, char ** argv)
+{
+
+    uint8_t  data[] = "\x60\x29\xA2\x05\x80\x03\x7B\xC1\x75\
+\xA6\x04\x80\x02\x7B\x04\xA8\x03\x02\x01\x07\
+\xBE\x15\x28\x13\x81\x11\x80\x0F\x50\x00\x02\
+\x55\x53\x45\x52\x20\x4E\x41\x4D\x45\x20\x00\x3C";
+
+    uint8_t data2[] = "\x60\x22\xa2\x04\x80\x02\x7b\x04\
+\xa4\x03\xa6\x05\x80\x03\x7b\xc1\x75\xa8\x03\x02\x01\x07\
+\xbe\x09\x28\x07\x81\x05\x80\x03\x00\x00\x3c";
+
+    C1222_Request_Logon logon (2, "USER NAME", 60);
+    uint8_t * d = logon.build(); // build data;
+    uint8_t * logonData = d;
+    printf("    [*] request data length : %lx\n", logon.get_build_size());
+
+    puts("");
+    C1222_EPSEM epsem (d, 0x80, 0, logon.get_build_size());
+    d = epsem.build();
+
+    printf("    [*] epsem length : %lx\n", epsem.get_length());
+    puts("");
+
+    C1222_ACSE acse ( d, "123.4", "7", "123.8437", NULL, epsem.get_length());
+    d = acse.build();
+
+    puts("");
+    printf("[!] FINAL\n");
+    for(int i = 0; i < acse.get_data_len(); ++i)
+        printf("0x%02x ", d[i]);
+
+    puts("");
+
+    C1222_Request_Logon * p = 
+        (C1222_Request_Logon*)C1222_Request_Parser::parse(logonData);
+
+    std::cout << "User id: " << p->get_user_id() << endl;
+    std::cout << "Timeout: " << p->get_timeout() << endl;
+    std::cout << "Username " << p->get_username() << endl;
+    
+    delete p;
+
+    return 0;
+}
